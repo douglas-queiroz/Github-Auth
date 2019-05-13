220217e7186853f576e97dbd4128f716aa6ace55
Douglas Queiroz <douglasq16@gmail.com>
2019-05-12 21:13:27 -0400

    Create getUserCredential at user dao

--- app/src/main/java/com/douglas/githubauth/data/local/UserDao.kt
+++ app/src/main/java/com/douglas/githubauth/data/local/UserDao.kt
@@ -5,4 +5,6 @@ import com.douglas.githubauth.domain.model.UserCredential
 interface UserDao {
 
     fun saveUserCredentials(userCredential: UserCredential) : Boolean
+
+    fun getUserCredential() : UserCredential
 }
