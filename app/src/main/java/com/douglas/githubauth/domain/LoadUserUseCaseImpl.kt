ee6b5c9b6cd9618a200c2bccf1af3a01f559c660
Douglas Queiroz <douglasq16@gmail.com>
2019-05-12 21:55:35 -0400

    Add LoadUserUseCase dependecies

--- app/src/main/java/com/douglas/githubauth/domain/LoadUserUseCaseImpl.kt
+++ app/src/main/java/com/douglas/githubauth/domain/LoadUserUseCaseImpl.kt
@@ -1,9 +1,14 @@
 package com.douglas.githubauth.domain
 
+import com.douglas.githubauth.data.local.UserDao
+import com.douglas.githubauth.data.remote.UserService
 import com.douglas.githubauth.domain.model.User
+import com.douglas.githubauth.util.AuthorizationUtil
 import rx.Observable
 
-class LoadUserUseCaseImpl: LoadUserUseCase {
+class LoadUserUseCaseImpl(private val userDao: UserDao,
+                          private val userService: UserService,
+                          private val authorizationUtil: AuthorizationUtil): LoadUserUseCase {
 
     override fun loadUser(): Observable<User> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
