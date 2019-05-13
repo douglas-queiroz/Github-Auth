9ce86b20effa091c27d3cb939f89dc2d1d70a4f7
Douglas Queiroz <douglasq16@gmail.com>
2019-05-13 10:46:35 -0400

    Create implementation of CoreViewModel

--- app/src/main/java/com/douglas/githubauth/module/core/CoreViewModel.kt
+++ app/src/main/java/com/douglas/githubauth/module/core/CoreViewModel.kt
@@ -1,10 +1,26 @@
 package com.douglas.githubauth.module.core
 
+import android.arch.lifecycle.MutableLiveData
+import com.douglas.githubauth.domain.CheckLoginUseCase
 import com.douglas.githubauth.module.base.BaseViewModel
 
-class CoreViewModel: BaseViewModel() {
+class CoreViewModel(private var checkLoginUseCase: CheckLoginUseCase): BaseViewModel() {
+
+    sealed class ViewState {
+        class ShowLoginScreen: ViewState()
+        class ShowProfileScreen: ViewState()
+    }
+
+    val viewState = MutableLiveData<ViewState>()
 
     fun checkIfHasUserLoggedIn() {
 
+        if (checkLoginUseCase.hasUserLogged()) {
+
+            viewState.value = ViewState.ShowProfileScreen()
+        } else {
+
+            viewState.value = ViewState.ShowLoginScreen()
+        }
     }
 }
