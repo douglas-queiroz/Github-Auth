7b41a3b303b41828bf1295439cf1a943e4c0dd42
Douglas Queiroz <douglasq16@gmail.com>
2019-05-13 10:29:05 -0400

    Create BaseViewModel

---
+++ app/src/main/java/com/douglas/githubauth/module/base/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.douglas.githubauth.module.base
+
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+class BaseViewModel: ViewModel(), LifecycleObserver {
+
+    val subscriptions = CompositeDisposable()
+    val loadingStatus = MutableLiveData<Boolean>()
+
+    override fun onCleared() {
+        super.onCleared()
+
+        subscriptions.clear()
+    }
+}
