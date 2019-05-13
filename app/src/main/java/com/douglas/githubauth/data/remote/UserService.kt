0d3afb21e44f57c6b4dee0256f6689fbcedd2d30
Douglas Queiroz <douglasq16@gmail.com>
2019-05-12 23:44:30 -0400

    Fix UserService

--- app/src/main/java/com/douglas/githubauth/data/remote/UserService.kt
+++ app/src/main/java/com/douglas/githubauth/data/remote/UserService.kt
@@ -3,11 +3,15 @@ package com.douglas.githubauth.data.remote
 import com.douglas.githubauth.domain.model.User
 import io.reactivex.Completable
 import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface UserService {
 
-    fun checkCredentials(authorization: String) : Completable
+    @GET("/")
+    fun checkCredentials(@Header("Authorization") authorization: String) : Completable
 
-    fun fetchUser(authorization: String) : Observable<User>
+    @GET("user")
+    fun fetchUser(@Header("Authorization") authorization: String) : Observable<User>
 
 }
