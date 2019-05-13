1a26927a1ac6d3a82e5f3b5344336f81658c6207
Douglas Queiroz <douglasq16@gmail.com>
2019-05-13 00:11:08 -0400

    Create implementation of UserLogOutUseCase's test case

--- app/src/test/java/com/douglas/githubauth/domain/UserLogOutUseCaseTest.kt
+++ app/src/test/java/com/douglas/githubauth/domain/UserLogOutUseCaseTest.kt
@@ -1,9 +1,10 @@
 package com.douglas.githubauth.domain
 
 import com.douglas.githubauth.data.local.UserDao
+import com.douglas.githubauth.domain.exception.WasNotAbleToRemoveUser
+import org.junit.Assert
 import org.junit.Before
-
-import org.junit.Assert.*
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
@@ -23,11 +24,29 @@ class UserLogOutUseCaseTest {
         target = UserLogOutUseCaseImpl(userDao)
     }
 
-    fun `When has no user logged in`() {
+    @Test
+    fun `When user dao was not able to remove use`() {
 
+        `when`(userDao.removeUser()).thenReturn(false)
+
+        try {
+
+            target.logOutUser()
+
+            Assert.fail()
+
+        } catch (error: Exception) {
+
+            Assert.assertTrue(error is WasNotAbleToRemoveUser)
+        }
     }
 
+    @Test
     fun `When log out user works fine`() {
+
+        `when`(userDao.removeUser()).thenReturn(true)
+
+        target.logOutUser()
 
     }
 }
