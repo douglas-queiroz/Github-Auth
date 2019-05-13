083bd4b16e39a27f99f4d67e06ac4b1aac7da655
Douglas Queiroz <douglasq16@gmail.com>
2019-05-12 21:02:55 -0400

    Create LoadUserUseCase interface and implementation class

--- app/src/main/java/com/douglas/githubauth/domain/LoadUserUseCaseImpl.kt
+++ app/src/main/java/com/douglas/githubauth/domain/LoadUserUseCaseImpl.kt
@@ -1,4 +1,11 @@
 package com.douglas.githubauth.domain
 
-class LoadUserUseCaseImpl {
+import com.douglas.githubauth.domain.model.User
+import rx.Observable
+
+class LoadUserUseCaseImpl: LoadUserUseCase {
+
+    override fun loadUser(): Observable<User> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
