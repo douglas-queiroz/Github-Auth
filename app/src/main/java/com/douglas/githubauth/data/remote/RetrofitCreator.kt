138bed98f5156cc633013d1907292a6655ffd229
Douglas Queiroz <douglasq16@gmail.com>
2019-05-12 22:44:20 -0400

    Create Retrofit Creator

--- app/src/main/java/com/douglas/githubauth/data/remote/RetrofitCreator.kt
+++ app/src/main/java/com/douglas/githubauth/data/remote/RetrofitCreator.kt
@@ -1,4 +1,34 @@
 package com.douglas.githubauth.data.remote
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitCreator {
+
+    companion object {
+
+        private fun provideGson(): Gson =
+            GsonBuilder()
+                .setLenient()
+                .create()
+
+        private fun provideOkHttpClient(): OkHttpClient =
+            OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build()
+
+        fun createRetrofit(url: String): Retrofit =
+            Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create(provideGson()))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .client(provideOkHttpClient())
+                .build()
+    }
 }
