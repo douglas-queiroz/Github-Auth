9378ae7e2c66fc91f1d6be877bfbe7fb55f35bc5
Douglas Queiroz <douglasq16@gmail.com>
2019-05-17 08:31:25 -0400

    Create LoginViewModel and its unit test

---
+++ app/src/main/java/com/douglas/githubauth/module/login/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.douglas.githubauth.module.login
+
+class LoginViewModel {
+
+    fun attempLogin(username: String, password: String) {
+
+    }
+}
---
+++ app/src/test/java/com/douglas/githubauth/module/login/LoginViewModelTest.kt
@@ -0,0 +1,45 @@
+package com.douglas.githubauth.module.login
+
+import com.douglas.githubauth.domain.UserLogInUseCase
+import com.douglas.githubauth.domain.UserLogOutUseCase
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class LoginViewModelTest {
+
+    @Mock
+    lateinit var userLoginUseCase: UserLogInUseCase
+
+    lateinit var target: LoginViewModel
+
+    @Before
+    fun setUp() {
+
+        MockitoAnnotations.initMocks(this)
+        target = LoginViewModel()
+    }
+
+    @Test
+    fun `When attempt login receives EmptyFieldException` () {
+
+    }
+
+    @Test
+    fun `When attempt login receives InvalidCredentialException` () {
+
+    }
+
+    @Test
+    fun `When attempt login receives WasNotAbleToSaveCredentialException` () {
+
+    }
+
+    @Test
+    fun `When attempt login works fine` () {
+
+    }
+}
