57d0e685fe9253c964a0050d0ed0f9263d58ce82
Douglas Queiroz <douglasq16@gmail.com>
2019-05-17 11:38:18 -0400

    Add LoginViewModel and LoginFragment onto Dagger setup

--- app/src/main/java/com/douglas/githubauth/di/module/ViewModelModule.kt
+++ app/src/main/java/com/douglas/githubauth/di/module/ViewModelModule.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProvider
 import com.douglas.githubauth.di.ViewModelFactory
 import com.douglas.githubauth.di.ViewModelKey
 import com.douglas.githubauth.module.core.CoreViewModel
+import com.douglas.githubauth.module.login.LoginViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -16,6 +17,11 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(CoreViewModel::class)
     internal abstract fun bindsCoreViewModel(coreViewModel: CoreViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginViewModel::class)
+    internal abstract fun bindsLoginViewModel(loginViewModel: LoginViewModel) : ViewModel
 
     @Binds
     internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
