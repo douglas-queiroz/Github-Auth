5a108085457ded495de17d04c909e2f905049bb9
Douglas Queiroz <douglasq16@gmail.com>
2019-05-16 21:48:47 -0400

    Create Dagger UseCaseModule

---
+++ app/src/main/java/com/douglas/githubauth/di/module/UseCaseModule.kt
@@ -0,0 +1,33 @@
+package com.douglas.githubauth.di.module
+
+import com.douglas.githubauth.data.local.UserDao
+import com.douglas.githubauth.data.remote.UserService
+import com.douglas.githubauth.domain.*
+import com.douglas.githubauth.helper.UserSessionHelper
+import com.douglas.githubauth.util.AuthorizationUtil
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class UseCaseModule {
+
+    @Singleton
+    @Provides
+    fun providesCheckLoginUseCase(userSessionHelper: UserSessionHelper) : CheckLoginUseCase
+            = CheckLoginUseCaseImpl(userSessionHelper)
+
+    @Singleton
+    @Provides
+    fun providesLoadUserUseCase(userDao: UserDao, userService: UserService, authorizationUtil: AuthorizationUtil) :
+            LoadUserUseCase = LoadUserUseCaseImpl(userDao, userService, authorizationUtil)
+
+    @Singleton
+    @Provides
+    fun providesUserLogInUseCase(userDao: UserDao, userService: UserService, authorizationUtil: AuthorizationUtil) :
+            UserLogInUseCase = UserLogInUseCaseImpl(userDao, userService, authorizationUtil)
+
+    @Singleton
+    @Provides
+    fun providesUserLogOut(userDao: UserDao) : UserLogOutUseCase = UserLogOutUseCaseImpl(userDao)
+}
