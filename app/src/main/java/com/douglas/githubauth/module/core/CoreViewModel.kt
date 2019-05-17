49a77ae502465547d5ba76c01522bcbdefcbd838
Douglas Queiroz <douglasq16@gmail.com>
2019-05-16 22:29:47 -0400

    Apply dependence injection to Core Module

--- app/src/main/java/com/douglas/githubauth/module/core/CoreViewModel.kt
+++ app/src/main/java/com/douglas/githubauth/module/core/CoreViewModel.kt
@@ -3,8 +3,10 @@ package com.douglas.githubauth.module.core
 import android.arch.lifecycle.MutableLiveData
 import com.douglas.githubauth.domain.CheckLoginUseCase
 import com.douglas.githubauth.module.base.BaseViewModel
+import javax.inject.Inject
 
-class CoreViewModel(private var checkLoginUseCase: CheckLoginUseCase): BaseViewModel() {
+class CoreViewModel
+    @Inject constructor(private var checkLoginUseCase: CheckLoginUseCase): BaseViewModel() {
 
     sealed class ViewState {
         class ShowLoginScreen: ViewState()
