6a9f8c4f035f6d25f2840db4d7f78d3e8c9f38ae
Douglas Queiroz <douglasq16@gmail.com>
2019-05-17 14:37:21 -0400

    Remove Bio and Add location field at User Model

--- app/src/main/java/com/douglas/githubauth/domain/model/User.kt
+++ app/src/main/java/com/douglas/githubauth/domain/model/User.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 open class User(val name: String,
                 val company: String,
                 val email: String,
-                val bio: String,
+                val location: String,
                 @SerializedName("avatar_url")
                 val avatarUrl: String)
