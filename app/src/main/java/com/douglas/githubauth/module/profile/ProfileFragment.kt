e34d8cb4ca78dd8ca6150b5299eb311fc2854a16
Douglas Queiroz <douglasq16@gmail.com>
2019-05-17 13:54:44 -0400

    Implements view status behaviors

--- app/src/main/java/com/douglas/githubauth/module/profile/ProfileFragment.kt
+++ app/src/main/java/com/douglas/githubauth/module/profile/ProfileFragment.kt
@@ -1,7 +1,12 @@
 package com.douglas.githubauth.module.profile
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import com.douglas.githubauth.Application
 import com.douglas.githubauth.R
+import com.douglas.githubauth.di.ViewModelFactory
+import com.douglas.githubauth.domain.model.User
 import com.douglas.githubauth.module.base.BaseFragment
 import com.douglas.githubauth.module.base.BaseViewModel
 
@@ -12,9 +17,45 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initDependece()
+    }
+
+    private fun initDependece() {
+
+        Application.component.inject(this)
+        viewModel = getViewModel()
+        subscribeViewModel()
+
+    }
+
+    private fun getViewModel() : ProfileViewModel {
+        return ViewModelProviders
+            .of(this, viewModelFactory)
+            .get(ProfileViewModel::class.java)
     }
 
     override fun getViewMode(): BaseViewModel {
         return viewModel
+    }
+
+    private fun subscribeViewModel() {
+        viewModel.viewState.observe(this, Observer { viewState ->
+            updateView(viewState)
+        })
+    }
+
+    private fun updateView(viewState: ProfileViewModel.ViewState?) {
+        when(viewState) {
+            is ProfileViewModel.ViewState.ShowProfile -> fillOutView(viewState.user)
+            is ProfileViewModel.ViewState.GoToLoginScreen -> goToLoginScreen()
+        }
+    }
+
+    private fun fillOutView(user: User) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun goToLoginScreen() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
